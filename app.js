69e4200d0d27a4798a8ddf1b0b0b7a535387e7d7
hashmat00 <info@hashmat00.com>
2017-01-12 20:33:05 +0000

    added user Schema

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require("express");
+var  morgan = require("morgan");
 
 var app = express();
 
 
+app.use(morgan('dev'));
 
+
+
+app.get('/', function(req, res){
+    res.json('homepage');
+});
+
+
+app.get('/catname', function(req, res){
+    res.json('added morgan');
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('server has started');
---
+++ models/user.js
@@ -0,0 +1,57 @@
+var mongoose = require("mongoose");
+var bcrypt = require("bcrypt");
+var passportLocalMongoose = require('passport-local-mongoose');
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema ({
+    email: { type: String, unique: true, lowercase: true},
+    password: String,
+    
+    profile: {
+        name: { type: String, default: ''},
+        picture: { type: String, default: ''}
+    },
+    
+    address: String,
+    history: [{
+        date: Date,
+        paid: { type: Number, default: 0},
+    }]
+});
+
+
+//===== USE THIS PASSPORT INSTEAD OF BCRYPT TO INCRYPT PASSWOR D=====
+// UserSchema.plugin(passportLocalMongoose);
+
+
+
+//===== USE BCRPT TO INCRYPT PASSWORD =====
+// hash the password with bcrypt
+UserSchema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('passwor')) return next();
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, null, function(err, hash){
+            if (err) return next();
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+
+
+// compare user password with db 
+UserSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+
+
+//===== USE function to set defautl username for all users when they sign up=====
+// UserSchema.pre('save', function(next){
+//     var user = this;
+//     user.name = 'specify name';
+// });
